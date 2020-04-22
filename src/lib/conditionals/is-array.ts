b6b545550c36e5b0efa823f66da3a3763050a568
Michael Coakley <mcoakley@acmeframework.com>
2020-04-22 17:12:51 -0400

    Optimize IsArray.test

--- src/lib/conditionals/is-array.ts
+++ src/lib/conditionals/is-array.ts
@@ -1,24 +1,23 @@
 import typeDetect from 'type-detect';
 
 import { assert_conditional, Conditional } from './conditional';
+import { isUndefinedOrNull } from './is-undefined-or-null';
 
 export class IsArray extends Conditional {
-    public test(value: any): boolean {
-        return typeDetect(value) !== 'undefined' &&
-            value !== null &&
-            typeDetect(value) === 'Array';
-    }
+  public test(value: any): boolean {
+    return !isUndefinedOrNull(value) && typeDetect(value) === 'Array';
+  }
 }
 
 export let isArrayInstance = new IsArray();
 
 export function isArray(value: any): boolean {
-    return isArrayInstance.test(value);
+  return isArrayInstance.test(value);
 }
 
 export function assert_isArray(
-    value: any,
-    assertMsg = 'is not an array.'
+  value: any,
+  assertMsg = 'is not an array.'
 ): void {
-    assert_conditional(value, isArrayInstance, assertMsg);
+  assert_conditional(value, isArrayInstance, assertMsg);
 }
